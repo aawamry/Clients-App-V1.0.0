dc72a25be022963ec4b45910d0018c4483f9d0d2
aawamry <a.awamry@live.com>
2025-06-20 19:28:00 +0300

    starting to integrate logger app

--- clients-backend/data/queries.js
+++ clients-backend/data/queries.js
@@ -1,4 +1,4 @@
-export const createClientsTable = () => `CREATE TABLE IF NOT EXISTS clients (
+export const createClientsTableQuery = () => `CREATE TABLE IF NOT EXISTS clients (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     firstName TEXT NOT NULL,
     middleName TEXT,
@@ -45,3 +45,25 @@ export const updateClientQuery = (table) =>
                          phone = ?, 
                          email = ? WHERE id = ?`;
 export const deleteClientQuery = (table) => `DELETE FROM ${table} WHERE id = ?`;
+
+export const createEventsLogTableQuery = () => `
+  CREATE TABLE IF NOT EXISTS logs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NOT NULL,
+    subject TEXT NOT NULL,
+    message TEXT NOT NULL,
+    user_id TEXT,
+    extra_data TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )
+`;
+
+export const getAllLogsQuery = `SELECT * FROM logs ORDER BY created_at DESC`;
+
+export const insertLogQuery = `
+  INSERT INTO logs (type, subject, message, user_id, extra_data)
+  VALUES (?, ?, ?, ?, ?)
+`;
+
+export const getLogByIdQuery = `SELECT * FROM logs WHERE id = ?`;
+
