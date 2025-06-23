f7e157624c15beb1e2d93c6432f2d5a8081f1c7d
aawamry <a.awamry@live.com>
2025-06-23 21:49:12 +0300

    fixing import csv routes

--- clients-backend/api/importerexporterapiroutes.js
+++ clients-backend/api/importerexporterapiroutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import upload from '../utilities/upload.js';
 import { exportClientsCSV } from '../utilities/csvexporter.js';
 import { importClientsCSV } from '../utilities/csvimporter.js';
 
@@ -8,6 +7,6 @@ const router = express.Router();
 
 router.get('/export-csv', exportClientsCSV);
 
-router.post('/import-csv', upload.single('file'), importClientsCSV);
+router.post('/import-csv', importClientsCSV);
 
 export default router;
