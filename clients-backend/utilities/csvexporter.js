982d4a808b68432a9bb4c48f848bbe78253108df
aawamry <a.awamry@live.com>
2025-06-24 19:23:30 +0300

    making export csv dynamic function

--- clients-backend/utilities/csvexporter.js
+++ clients-backend/utilities/csvexporter.js
@@ -1,38 +1,34 @@
 import { Parser } from 'json2csv';
-import {
-	getAllClientsModel,
-} from '../models/models.js';
-import { logEvent } from '../utilities/logger.js'
+import { getAllClientsModel } from '../models/models.js';
+import { logEvent } from '../utilities/logger.js';
 
 export const exportClientsCSV = async (req, res) => {
 	try {
-		const clients = await getAllClientsModel(); // your DB fetch function
+		const records = await getAllClientsModel(); // your DB fetch function
 
-		const fields = [
-			'id',
-			'firstName',
-			'lastName',
-			'companyName',
-			'address',
-			'region',
-			'city',
-			'nationality',
-			'dateOfBirth',
-			'gender',
-			'phone',
-			'email',
-			'created_at'
-		];
+		const fields = Array.from(new Set(records.flatMap(Object.keys)));
 		const parser = new Parser({ fields });
 		const csv = parser.parse(clients);
 		const fileName = `clients_export_${new Date().toISOString().slice(0, 10)}.csv`;
-		await logEvent({event_type:'WARNING', event_subject:'Clients Export Succeeded', event_message:`Exported ${clients.length} clients to CSV`});
+		await logEvent({
+			event_type: 'WARNING',
+			event_subject: 'Clients Export Succeeded',
+			event_message: `Exported ${clients.length} clients to CSV`
+		});
 		res.header('Content-Type', 'text/csv');
 		res.attachment(fileName);
-		await logEvent({event_type:'WARNING',event_subject:'SENT', event_message:`Sent ${clients.length} clients to CSV`});
+		await logEvent({
+			event_type: 'WARNING',
+			event_subject: 'SENT',
+			event_message: `Sent ${clients.length} clients to CSV`
+		});
 		return res.send(csv);
 	} catch (error) {
-		await logEvent({event_type:'ERROR', event_subject:'Clients Export Failed', event_message:`CSV export failed: ${error.message}`});
+		await logEvent({
+			event_type: 'ERROR',
+			event_subject: 'Clients Export Failed',
+			event_message: `CSV export failed: ${error.message}`
+		});
 		res.status(500).send('Failed to export clients.');
 	}
 };
